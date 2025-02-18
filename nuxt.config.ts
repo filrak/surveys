899d2e67796bf6d4d5ef4f91628ba70ff701dd7d
Filip <f.rakowski@hotmail.com>
2025-02-18 11:30:51 +0100

    udpate env vars

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,15 +4,26 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   modules: ['@nuxtjs/tailwindcss', 'nuxt-vuefire', 'shadcn-nuxt'],
+  runtimeConfig: {
+    public: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID
+    }
+  },
   vuefire: {
     config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+      apiKey: '#{public.firebaseApiKey}',
+      authDomain: '#{public.firebaseAuthDomain}',
+      projectId: '#{public.firebaseProjectId}',
+      storageBucket: '#{public.firebaseStorageBucket}',
+      messagingSenderId: '#{public.firebaseMessagingSenderId}',
+      appId: '#{public.firebaseAppId}',
+      measurementId: '#{public.firebaseMeasurementId}'
     },
     auth: {
       enabled: true
